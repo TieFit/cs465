a957b255f1c620e73ad41e2e54ddf4c0acd02755
JJ Newell <jonathonnewell123@gmail.com>
2025-10-12 00:41:38 -0400

    Module 6 completed baseline

--- travlr/app_api/controllers/trips.js
+++ travlr/app_api/controllers/trips.js
@@ -48,7 +48,80 @@ const tripsFindByCode = async(req, res) => {
     }
 };
 
+const tripsAddTrip = async(req, res) => {
+    const newTrip = new Trip({
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+
+    });
+
+    const q = await newTrip.save();
+        if (!q) {
+            //database returned no data
+            return res  
+                .status(400)
+                .json(err)
+
+        } else { // return trip
+            return res
+                .status(201)
+                .json(q)
+        }
+
+        // uncomment the following line to show results operation on the console
+        //console.log(q);
+};
+
+// PUT: /trips/:tripCode - Updates an existing Trip
+// Regardless of outcome, response must include HTML status code
+// and a JSON message to the requesting client
+const tripsUpdateTrip = async (req, res) => {
+  // Uncomment for debugging
+  console.log(req.params);
+  console.log(req.body);
+
+  try {
+    const q = await Model.findOneAndUpdate(
+      { code: req.params.tripCode },
+      {
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+      },
+      { new: true } // return the updated document
+    ).exec();
+
+    if (!q) {
+      // Database returned no data
+      return res.status(400).json({ message: "Trip not found or update failed." });
+    } else {
+      // Return resulting updated trip
+      return res.status(201).json(q);
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Server error", error: err });
+  }
+
+  // Uncomment the following line to show results of operation on the console
+  // console.log(q);
+};
+
+
 module.exports = {
     tripsList,
-    tripsFindByCode
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
